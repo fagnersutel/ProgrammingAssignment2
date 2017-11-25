7a6973cb8b4cc0c30438a44dfb70eee476013c4a
Fagner Sutel <fagnersutel@hotmail.coml>
2017-11-25 16:07:50 -0200

    Segundo commit do make cache matrix

--- make.R
+++ make.R
@@ -3,8 +3,27 @@
 # Para este fim ser?? utilizado o operador de atribui;??o <<-que serve para atribuir valores a um objeto em ambientes diferente do atual.
 #O Exemplo do exerc??cio demonstra a fun;??omakeVector que cria o vetor 
 makeCacheMatrix <- function(x = matrix()) {
-    
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setmean <- function(mean) m <<- mean
+    getmean <- function() m
+    list(set = set, get = get,
+         setmean = setmean,
+         getmean = getmean)
 }
 cacheSolve <- function(x = matrix()) {
+    m <- x$getmean()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- mean(data, ...)
+    x$setmean(m)
+    m
 
     }
