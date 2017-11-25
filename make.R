58751845ab0e781c17903d714f5aeb8811c703a2
Fagner Sutel <fagnersutel@hotmail.coml>
2017-11-25 16:06:17 -0200

    commit do make cache matrix

---
+++ make.R
@@ -0,0 +1,10 @@
+#primeira tarefa da segunda subimiss??o onde criamos a fun????oo makeCacheMatrix
+#A ideia ?? criar um array que ir?? armazenar os dados computados das medias de tempo de consumo precedentes
+# Para este fim ser?? utilizado o operador de atribui;??o <<-que serve para atribuir valores a um objeto em ambientes diferente do atual.
+#O Exemplo do exerc??cio demonstra a fun;??omakeVector que cria o vetor 
+makeCacheMatrix <- function(x = matrix()) {
+    
+}
+cacheSolve <- function(x = matrix()) {
+
+    }
