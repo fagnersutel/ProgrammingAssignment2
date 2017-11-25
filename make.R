1adcda5690709c56e403248acf77c3b8581490e2
Fagner Sutel <fagnersutel@hotmail.coml>
2017-11-25 17:09:01 -0200

    Teste final 2 commit do make cache matrix

--- make.R
+++ make.R
@@ -1,4 +1,4 @@
-#primeira tarefa da segunda subimiss??o onde criamos a fun????o makeCacheMatrix
+#primeira tarefa da segunda subimisao onde criamos a funcao makeCacheMatrix
 #A ideia ?? criar um array que ir?? armazenar os dados computados das medias de tempo de consumo precedentes
 # Para este fim ser?? utilizado o operador de atribui????o <<-que serve para atribuir valores a um objeto em ambientes diferente do atual.
 #O Exemplo do exerc??cio demonstra a fun????o makeVector que cria o vetor 
