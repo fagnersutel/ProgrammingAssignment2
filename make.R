2a107fab0da5249d9ba7eb0cc4299d8d2fcf0651
Fagner Sutel <fagnersutel@hotmail.coml>
2017-11-25 16:19:48 -0200

    Terceiro commit do make cache matrix

--- make.R
+++ make.R
@@ -1,29 +1,31 @@
-#primeira tarefa da segunda subimiss??o onde criamos a fun????oo makeCacheMatrix
+#primeira tarefa da segunda subimiss??o onde criamos a fun????o makeCacheMatrix
 #A ideia ?? criar um array que ir?? armazenar os dados computados das medias de tempo de consumo precedentes
-# Para este fim ser?? utilizado o operador de atribui;??o <<-que serve para atribuir valores a um objeto em ambientes diferente do atual.
-#O Exemplo do exerc??cio demonstra a fun;??omakeVector que cria o vetor 
+# Para este fim ser?? utilizado o operador de atribui????o <<-que serve para atribuir valores a um objeto em ambientes diferente do atual.
+#O Exemplo do exerc??cio demonstra a fun????o makeVector que cria o vetor 
+#Como no exerc??cio de exemplo a fun????o se chamava 'makeVector' e agora chamaremos de 'makeCacheMatrix'
+
 makeCacheMatrix <- function(x = matrix()) {
-    m <- NULL
-    set <- function(y) {
-        x <<- y
-        m <<- NULL
+    ma <- NULL # permanece como no exemplo original
+    set <- function(y) { #repretimso a fun;??o do exemplo original
+        x <<- y #mesma variavel do exemplo original
+        ma <<- NULL #mesma variavel do exemplo original
     }
-    get <- function() x
-    setmean <- function(mean) m <<- mean
+    get <- function() x  #mesma variavel e fun????o do exemplo original
+    setmean <- function(mean) ma <<- mean
     getmean <- function() m
     list(set = set, get = get,
          setmean = setmean,
          getmean = getmean)
 }
 cacheSolve <- function(x = matrix()) {
-    m <- x$getmean()
-    if(!is.null(m)) {
+    ma <- x$getmean()
+    if(!is.null(ma)) {
         message("getting cached data")
         return(m)
     }
     data <- x$get()
-    m <- mean(data, ...)
-    x$setmean(m)
-    m
+    ma <- mean(data, ...)
+    x$setmean(ma)
+    ma
 
     }
